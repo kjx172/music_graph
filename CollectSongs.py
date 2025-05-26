930ad5416e9c665be95f69f6ee757c5446b5d5ff
Aaron Kisitu <kisituaaron@gmail.com>
2025-05-25 19:51:02 -0500

    now able to get users entire playlist tracklist

--- CollectSongs.py
+++ CollectSongs.py
@@ -2,10 +2,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 from Auth import spotify_connection
-from GatherPlaylists import get_playlists
+from GatherPlaylists import get_playlists, get_total_user_tracks
 
 # Run authorization function to get connection
 sp = spotify_connection()
 
 # Gets the list of user playlists based on username
-get_playlists(sp)
+user_playlists = get_playlists(sp)
+
+# Gets a list of songs in each playlist
+get_total_user_tracks(sp, user_playlists)
--- GatherPlaylists.py
+++ GatherPlaylists.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
 
 def get_playlists(sp):
     print("Enter Spotify username (user ID): ")
@@ -10,12 +11,68 @@ def get_playlists(sp):
 
         # Attempt to fetch user’s playlists
         try:
-            results = sp.user_playlists(username)
+            user_playlists = sp.user_playlists(username)
             break
         except:
             print("Error: please enter valid username")
 
+    return user_playlists
+
+    '''
     # Extract and print playlist names
     print(f"\nPlaylists for {username}:")
-    for idx, item in enumerate(results['items']):
+    for idx, item in enumerate(user_playlists['items']):
         print(f"{idx + 1}. {item['name']} ({item['tracks']['total']} tracks)")
+    '''
+
+def get_all_tracks_from_playlist(sp, playlist_id):
+    """Helper function to fetch all tracks from a playlist with pagination."""
+    all_tracks = []
+    offset = 0
+    while True:
+        response = sp.playlist_tracks(playlist_id, limit=100, offset=offset)
+        items = response['items']
+        if not items:
+            break
+        all_tracks.extend(items)
+        offset += 100
+    return all_tracks
+
+def get_total_user_tracks(sp, user_playlists):
+    # Storage
+    track_set = set()  # To prevent duplicates
+    track_data = []    # To build a DataFrame
+
+    # Loop through each playlist in the users list of playlists
+    for playlist in user_playlists['items']:
+        
+        # Get the playlist id for this playlist and extrack tracks
+        playlist_id = playlist['id']
+
+        # Fetch ALL tracks from this playlist, even if it has > 100
+        playlist_tracks = get_all_tracks_from_playlist(sp, playlist_id)
+
+        # For each track in the playlists tracklist
+        for item in playlist_tracks:
+            # Get the track
+            track = item['track']
+            if not track:
+                continue  # skip if local file or deleted
+            
+            # Get track id and add it to list if not seen before
+            track_id = track['id']
+            if track_id and track_id not in track_set:
+                track_set.add(track_id)
+                track_data.append({
+                    'track_name': track['name'],
+                    'artist': track['artists'][0]['name'],
+                    'album': track['album']['name'],
+                    'track_id': track_id
+                })
+    # Convert to DataFrame
+    df = pd.DataFrame(track_data)
+
+    # Example export
+    df.to_csv('unique_user_tracks.csv', index=False)
+
+    print(f"Collected {len(df)} unique tracks.")
